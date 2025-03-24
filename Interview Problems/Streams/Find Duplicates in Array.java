6d5935a23e088c7b78f0d776a5f87048c5885a28
PRADY <rocks.pradeep014@gmail.com>
2025-03-24 15:44:21 +0530

    Update Find Duplicates in Array.java

--- Interview Problems/Streams/Find Duplicates in Array.java
+++ Interview Problems/Streams/Find Duplicates in Array.java
@@ -3,18 +3,14 @@ import java.util.stream.*;
 
 class Main {
     public static void main(String[] args) {
-        int arr[] = {2,4,5,6,2,4,4};
+        int arr[] = {2,4,5,6,2,4,6,4};
         Set<Integer> set = new HashSet<>();
 
+        Arrays.stream(arr)
+            .filter(num -> !set.add(num))
+            .distinct()
+            .boxed()
+            .forEach(System.out::println);
         
-        List<Integer> array = Arrays.stream(arr)
-        .filter(num -> !set.add(num))
-        .distinct()
-        .boxed()
-        .collect(Collectors.toList());
-        
-        for(Integer i: array){
-            System.out.println(i);
-        }
     }
 }
